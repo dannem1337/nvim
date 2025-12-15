bb74015df933d06e7ab8b300e4810c3089357166
Danyal Mirza <danyalmirza@gmail.com>
2025-12-15 10:19:36 +0100

    config: disable termguicolors and enable autoread
    - Disable termguicolors for better terminal compatibility
    - Enable autoread to automatically reload files changed outside vim

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -11,6 +11,7 @@ vim.opt.smartindent = true
 vim.opt.clipboard = "unnamedplus"
 
 vim.opt.swapfile = false
+vim.opt.autoread = true
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
@@ -18,7 +19,7 @@ vim.opt.undofile = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
-vim.opt.termguicolors = true
+vim.opt.termguicolors = false
 
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
