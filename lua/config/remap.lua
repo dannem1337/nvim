9a063d070c867b39fe84f9a54e30502c14111a3e
Danyal Mirza <danyalmirza@gmail.com>
2025-12-15 10:19:36 +0100

    feat: add git merge conflict navigation keymap
    - Add <leader>gm keymap to navigate to git merge conflict markers
    - Comment out unused window split keymaps

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,7 +1,8 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]])
-vim.keymap.set('n', '<A-w><A-v>', vim.cmd.vsplit)
-vim.keymap.set('n', '<A-w><A-w>', '<C-w><C-w>', { noremap = true })
+vim.keymap.set('n', '<leader>gm', [[/\v^\<\<\<\<\<\<\< \|\=\=\=\=\=\=\=$\|\>\>\>\>\>\>\> /<cr>]], { noremap = true, silent = true })
+-- vim.keymap.set('n', '<A-w><A-v>', vim.cmd.vsplit)
+-- vim.keymap.set('n', '<A-w><A-w>', '<C-w><C-w>', { noremap = true })
 local bufopts = { noremap = true, silent = true, buffer = bufnr }
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, bufopts)
